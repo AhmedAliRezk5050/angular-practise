2986d49ea595bbf9ce883ca6ec16d0596f45c35a
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-09-29 22:36:43 +0200

    observables(interval)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {interval, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  private sub?: Subscription
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.sub = interval(1000)
+      .subscribe(count => console.log(count));
+  }
+
+  ngOnDestroy(): void {
+    this.sub?.unsubscribe()
   }
 
 }
