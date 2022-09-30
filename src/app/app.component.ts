17473b1fd61d4ad0f14d0df222db429192c4f644
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-09-30 22:36:17 +0200

    forms(init)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,12 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import UserService from "./user.service";
-import {Subscription} from "rxjs";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  isActivated = false
-  obs?: Subscription
-  constructor(private userService: UserService) {}
-
-  ngOnInit() {
-    this.obs = this.userService.userEventEmitter.subscribe(data => {
-      this.isActivated = data
-    })
-  }
-
-  ngOnDestroy(): void {
-    this.obs?.unsubscribe();
+export class AppComponent {
+  suggestUserName() {
+    const suggestedName = 'Superuser';
   }
 }
--- src/app/user.service.ts
+++
@@ -1,7 +0,0 @@
-import {EventEmitter, Injectable} from "@angular/core";
-import {Subject} from "rxjs";
-
-@Injectable({providedIn: 'root'})
-export default class UserService {
-  userEventEmitter = new Subject<boolean>()
-}
