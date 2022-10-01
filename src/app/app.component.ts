f16482a2289ea294bb306a491c47c85a286f73a5
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-10-01 12:28:54 +0200

    cleaning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,53 +7,9 @@ import {NgForm} from "@angular/forms";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f') userForm?: NgForm
-
   genders = ['male', 'female']
 
-  answer = '';
 
-  defaultQuestion = "teacher"
 
-  user?:FormData
-
-  submitted = false
-
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-    /// resets other form value
-    // this.userForm?.setValue({
-    //   "userData": {
-    //     "username": suggestedName,
-    //     "email": ""
-    //   },
-    //   "secret": "",
-    //   "reply": "",
-    //   "gender": ""
-    // });
-
-    this.userForm?.form.patchValue({
-      userData: {
-        username: suggestedName
-      }
-    });
-  }
-
-  onSubmit() {
-    this.submitted = true;
-    this.user = this.userForm?.value
-    this.userForm?.reset();
-  }
 }
 
-interface UserData {
-  username: string;
-  email: string;
-}
-
-interface FormData {
-  userData: UserData;
-  secret: string;
-  reply: string;
-  gender: string;
-}
