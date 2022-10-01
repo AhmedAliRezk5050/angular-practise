06913a63a8c3be4fb50f74c37b8c7ef28f1b337a
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-10-01 14:38:23 +0200

    forms(Reactive)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
-import {Component, ViewChild} from '@angular/core';
-import {NgForm} from "@angular/forms";
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {FormControl, FormGroup, NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   genders = ['male', 'female']
 
+  userForm!: FormGroup
 
+  ngOnInit(): void {
+    this.userForm = new FormGroup({
+      'username': new FormControl(null),
+      'email': new FormControl(null),
+      'gender': new FormControl('male'),
+    })
+  }
+
+  onSubmit() {
+    console.log(this.userForm);
+  }
 
 }
 
