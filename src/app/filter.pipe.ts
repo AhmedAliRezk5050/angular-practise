a2b0673d67b3b9511ece9bf685ab8d05dadb9f04
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-10-02 16:49:22 +0200

    pipes(example => filter Custom Pipe)

---
+++ src/app/filter.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(item: any, filterBy: string, filterValue: string) {
+    if(item.length  === 0 || filterValue === '') return item;
+
+    const resultArray = [];
+    for (let f of item) {
+        if(f[filterBy] === filterValue) {
+          resultArray.push(f);
+        }
+    }
+
+    return resultArray
+  }
+
+}
