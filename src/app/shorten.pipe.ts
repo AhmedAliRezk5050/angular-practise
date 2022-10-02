104100c30cb092933d5f6825d1364774aad76e4d
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-10-02 16:17:56 +0200

    pipes(Custom Pipe) => fix

--- src/app/shorten.pipe.ts
+++ src/app/shorten.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ShortenPipe implements PipeTransform {
 
   transform(value: any){
-    return value.length > 7 ? value + '...' : value;
+    return value.length > 7 ? value.substring(0, 7) + '...' : value;
   }
 
 }
