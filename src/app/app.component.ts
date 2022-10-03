75d7071a65639bbdb79323da7b7d90c69c04e6eb
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-10-03 13:23:04 +0200

    http(get)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,7 +22,7 @@ export class AppComponent implements OnInit {
   }
 
   onFetchPosts() {
-    // Send Http request
+    this.http.get(this.url).subscribe(res => console.log(res))
   }
 
   onClearPosts() {
