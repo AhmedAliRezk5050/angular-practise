56d87e0a21cc31bf44240476ecfc466e216a98c9
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-10-03 13:56:00 +0200

    http(Using RxJS Operators to Transform Response Data)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[] = [];
 
   private url = 'https://angular-practise-caf1f-default-rtdb.firebaseio.com/posts.json'
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
@@ -22,10 +26,26 @@ export class AppComponent implements OnInit {
   }
 
   onFetchPosts() {
-    this.http.get(this.url).subscribe(res => console.log(res))
+    this.http.get(this.url)
+      .pipe(map((data: { [s: string]: any }) => {
+        const posts: Post[] = [];
+        for (const key in data) {
+          if (data.hasOwnProperty(key)) {
+            posts.push({...data[key], id: key})
+          }
+        }
+        return posts;
+      }))
+      .subscribe((posts: Post[]) => this.loadedPosts = posts)
   }
 
   onClearPosts() {
     // Send Http request
   }
+}
+
+interface Post {
+  id: string;
+  title: string;
+  content: string;
 }
